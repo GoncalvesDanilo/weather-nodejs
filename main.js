ba2a03d0b7b1f38cb869daa67e9f1e546e2c17f4
Danilo Goncalves <57415871+GoncalvesDanilo@users.noreply.github.com>
2021-02-01 22:13:14 -0300

    My commit

---
+++ main.js
@@ -0,0 +1,52 @@
+const request = require("request")
+const dotenv = require("dotenv").config()
+const prompt = require('prompt-sync')()
+
+const city = prompt("Search city: ")
+
+main()
+
+function main(){
+    geocoding(city)
+}
+
+//Finds latitude and longitude using LocationIQ API
+function geocoding(city){
+    const url = "https://us1.locationiq.com/v1/search.php?key="+ process.env.GEO_API_KEY +"&q="+ city +"&format=json"
+
+    if(!city){
+        return console.log("\nPlease enter the name of the city. \n(e.g. 'node main.js Jundiai,BR\n)")
+    }
+
+    request(url, (error, response, body) => {
+        const data = JSON.parse(body)
+        
+        if(!data[0]){
+            return console.log("Please enter the name of the city. \n(e.g. 'node main.js Jundiai,BR)")
+        }
+        lat = (data[0].lat)
+        lon = (data[0].lon)
+
+        weather(lat,lon)
+    })
+
+}
+
+//Gets weather info from OpenWeather API
+function weather(lat,lon){
+    const url = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&exclude=minutely,hourly,daily&units=metric&appid="+ process.env.WEATHER_API_KEY
+
+    request(url, (error, response, body) => {
+        const data = JSON.parse(body)
+        console.log("\n-=Weather in "+ city +"=-")
+        console.log(data.current.weather[0].main+" - "+data.current.weather[0].description)
+        console.log("\nTemperature: "+ data.current.temp +"°C")
+        console.log("Feels like: "+ data.current.feels_like +"°C")
+        console.log("Humidity: "+ data.current.humidity +"%")
+        console.log("Wind: "+ data.current.wind_speed +"km/h\n")
+        if(data.alerts){
+            console.log("Alert from "+ data.alerts[0].sender_name+": "+data.alerts[0].event)
+            console.log(" -> "+ data.alerts[0].description+"\n\n")
+        }
+    })
+}
